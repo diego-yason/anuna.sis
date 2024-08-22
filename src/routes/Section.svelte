0beef0178dac2c04ca41953e9bc9d8eaebf5fe8c
KingMarine-GH <61825575+KingMarine-GH@users.noreply.github.com>
2024-08-22 17:32:48 +0800

    added section component for initial props

---
+++ src/routes/Section.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	import type { CourseSection } from '$lib/Archer.d';
+
+	export let data: CourseSection;
+	const { classNumber, courseCode, capacity, enrolled, faculty, remarks, schedule, section } = data;
+</script>
+
+<div></div>
