fc05f41b5a1b9e1624cff3af6346ced5bcc346ce
KingMarine-GH <61825575+KingMarine-GH@users.noreply.github.com>
2024-08-22 17:32:31 +0800

    added fetcher for sections

--- src/routes/Slots.svelte
+++ src/routes/Slots.svelte
@@ -1,7 +1,26 @@
 <script lang="ts">
 	import { getClass } from '$lib/archer';
+	import type { CourseSection } from '$lib/Archer.d';
+	import { writable, type Writable } from 'svelte/store';
 
-	getClass('CCPROG3').then((res) => {
-		console.table(res);
-	});
+	let code = '',
+		isFetching = false;
+	const sections: Writable<CourseSection[]> = writable([]);
+
+	async function fetchSections() {
+		isFetching = true;
+		sections.set(await getClass(code));
+		isFetching = false;
+	}
 </script>
+
+<form on:submit|preventDefault={fetchSections}>
+	<input type="text" placeholder="Course Code" bind:value={code} />
+	<button disabled={isFetching} type="submit">Submit</button>
+</form>
+
+<div>
+	{#each $sections as section}
+		{section.classNumber}
+	{/each}
+</div>
