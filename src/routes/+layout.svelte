1428c36ad562c28ebd0cb817bf1d8ab010972235
KingMarine-GH <61825575+KingMarine-GH@users.noreply.github.com>
2024-12-06 00:13:20 +0800

    removie

--- src/routes/+layout.svelte
+++
@@ -1,7 +0,0 @@
-<script lang="ts">
-	import '../app.css';
-	import "../dlsu-css/default.css";
-	let { children } = $props();
-</script>
-
-{@render children()}
--- src/routes/+page.svelte
+++
@@ -1,6 +0,0 @@
-<script lang="ts">
-    import SectionCard from "$lib/components/SectionCard.svelte";
-    import sampleData from "$lib/sampleData.json";
-</script>
-
-<SectionCard {...sampleData}></SectionCard>
