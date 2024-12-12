8b842e857f3b1d826db08fa66a2e9cc7c643e7b5
KingMarine-GH <61825575+KingMarine-GH@users.noreply.github.com>
2024-12-12 20:51:56 +0800

    reset migration path

--- supabase/migrations/0000_overjoyed_wilson_fisk.sql
+++ supabase/migrations/0000_overjoyed_wilson_fisk.sql
@@ -1,13 +1,13 @@
 CREATE TYPE "public"."requisiteEnum" AS ENUM('H', 'S', 'C');--> statement-breakpoint
 CREATE TABLE "courseEquivalences" (
-	"courseA" varchar(7) PRIMARY KEY NOT NULL,
-	"courseB" varchar(7) PRIMARY KEY NOT NULL
+	"courseA" varchar(7),
+	"courseB" varchar(7)
 );
 --> statement-breakpoint
 ALTER TABLE "courseEquivalences" ENABLE ROW LEVEL SECURITY;--> statement-breakpoint
 CREATE TABLE "courseRequisites" (
-	"requisite" varchar(7) PRIMARY KEY NOT NULL,
-	"courseCode" varchar(7) PRIMARY KEY NOT NULL,
+	"requisite" varchar(7),
+	"courseCode" varchar(7),
 	"type" "requisiteEnum" NOT NULL
 );
 --> statement-breakpoint
@@ -27,11 +27,16 @@ CREATE TABLE "curriculums" (
 --> statement-breakpoint
 ALTER TABLE "curriculums" ENABLE ROW LEVEL SECURITY;--> statement-breakpoint
 CREATE TABLE "personalSchedules" (
-	"schedule" integer PRIMARY KEY NOT NULL,
-	"classNumber" integer PRIMARY KEY NOT NULL
+	"schedule" integer,
+	"classNumber" integer
 );
 --> statement-breakpoint
 ALTER TABLE "personalSchedules" ENABLE ROW LEVEL SECURITY;--> statement-breakpoint
+CREATE TABLE "REF_programs" (
+	"name" text PRIMARY KEY NOT NULL
+);
+--> statement-breakpoint
+ALTER TABLE "REF_programs" ENABLE ROW LEVEL SECURITY;--> statement-breakpoint
 CREATE TABLE "schedules" (
 	"id" serial PRIMARY KEY NOT NULL,
 	"owner" uuid NOT NULL,
@@ -51,8 +56,8 @@ CREATE TABLE "sectionSchedules" (
 );
 --> statement-breakpoint
 CREATE TABLE "sections" (
-	"classNumber" integer PRIMARY KEY NOT NULL,
-	"term" integer PRIMARY KEY NOT NULL,
+	"classNumber" integer,
+	"term" integer,
 	"section" varchar NOT NULL,
 	"courseCode" varchar(7) NOT NULL,
 	"capacity" integer NOT NULL,
@@ -68,4 +73,14 @@ CREATE TABLE "terms" (
 	"end" date NOT NULL
 );
 --> statement-breakpoint
-ALTER TABLE "terms" ENABLE ROW LEVEL SECURITY;
+ALTER TABLE "terms" ENABLE ROW LEVEL SECURITY;--> statement-breakpoint
+CREATE TABLE "users" (
+	"uid" uuid PRIMARY KEY NOT NULL,
+	"idNumber" integer,
+	"username" text NOT NULL,
+	"displayName" text,
+	"major" text,
+	"minor" text
+);
+--> statement-breakpoint
+ALTER TABLE "users" ENABLE ROW LEVEL SECURITY;
